aef41f0eaa4dba9fdad23df7e8d8659bfd428b98
Tristan Wiley <tristan@tristanwiley.com>
2017-06-08 17:00:54 -0400

    Admin page

---
+++ admin/admin.js
@@ -0,0 +1,6 @@
+document.getElementById("submitButton").addEventListener("click", function() {
+    var url = "http://tristanwiley.com/joey5802/tenessee/api.php?text=" + document.getElementById("howudoingman").value;
+    fetch(url).then(data => data.text()).then(data => {
+        console.log("SUBMITTED" + url);
+    });
+});
