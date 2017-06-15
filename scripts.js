14568fa2983ff7c7e52b249cdcdcc20775453264
Tristan Wiley <tristan@tristanwiley.com>
2017-06-15 15:47:38 -0400

    Create scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
 fetch("http://tristanwiley.com/joey5802/tenessee/get.php").then(data => data.text()).then(data => {
-    document.getElementById("howdoing").innerHTML = data.toLowerCase();
+    document.getElementById("howdoing").innerHTML = data.toLowerCase().replace(/&/g, '&amp;').replace(/</g, '&lt;');
 });
